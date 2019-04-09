dc0cf0a5c227c40b77fbc78a5df56cae4722a459
Joshua Rodriguez <jrodriguez332@cnm.edu>
2019-04-09 14:10:15 -0600

    made functions

--- Snap_04_09.js
+++ Snap_04_09.js
@@ -1,3 +1,43 @@
-function Multiply (47, 29) {
-	return 47 * 29;
+/**
+ * Take two numbers and multiply them.
+ *
+ * @param int firstNumber One number to multiply.
+ * @param int secondNumber The other number to add
+ * @return int The product of the two numbers.
+ */
+function multiplyNumbers(firstNumber, secondNumber) {
+	return firstNumber * secondNumber;
 }
+
+let myNumber = -5;
+let firstNumber = 29;
+
+console.log(firstNumber);
+
+console.log(multiplyNumbers(-2, myNumber));
+console.log(multiplyNumbers(2, myNumber));
+console.log(multiplyNumbers(-1, 5));
+console.log(multiplyNumbers(6, 487635789));
+
+console.log(firstNumber);
+
+
+/**
+ * Take two strings and concatenate them into a full name/
+ *
+ * @param string firstName The given name to concatenate
+ * @param string lastName The family name to concatenate
+ * @return string The full name of our person.
+ */
+function getFullName (firstName, lastName) {
+	return firstName + ' ' + lastName;
+}
+
+let myFirstName = 'Barry';
+
+console.log(getFullName(myFirstName, 'Schulzetenberg'));
+console.log(getFullName("Paul", "Schulzetenberg"));
+
+
+
+console.log(myFirstName, 'Schulzetenberg');
