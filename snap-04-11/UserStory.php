70b1a2f5b92239de5b248fa630392f2274bfffc0
Joshua Rodriguez <jrodriguez332@cnm.edu>
2019-04-11 09:04:02 -0600

    Added user story and screenshot.

---
+++ snap-04-11/UserStory.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang ="en">
+	<head>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<h1><b>User Story:</b></h1>
+		<div>
+			<li>As a user I click on the content section of the Campaign Builder.</li>
+			<li>As a user the server takes me to the edit design section.</li>
+			<li>As a user on the Design section I click the image content block I want
+			to work with.</li>
+			<li>As a user I click browse which the server takes me to the Content Manager.</li>
+			<li>As a user I click the tab which contains my image.</li>
+			<li>As a user I click the image that I want to work with and press insert
+			which adds my image.</li>
+			<li>As a user I finally click save & close which means the image is set.</li>
+		</div>
+	</body>
